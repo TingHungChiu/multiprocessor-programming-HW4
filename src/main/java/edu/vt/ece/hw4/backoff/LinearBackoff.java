4a45f31c271d94ebd50238151a2b06340d9351d6
TingHungChiu <113576156+TingHungChiu@users.noreply.github.com>
2022-10-30 09:20:03 -0400

    Update LinearBackoff.java

--- src/main/java/edu/vt/ece/hw4/backoff/LinearBackoff.java
+++ src/main/java/edu/vt/ece/hw4/backoff/LinearBackoff.java
@@ -7,7 +7,6 @@ public class LinearBackoff implements Backoff{
     public void backoff() throws InterruptedException {
         a = count;
 
-        //System.out.println("a:"+count);
         int delay = (int)a;
         System.out.println("delay:"+delay);
         Thread.sleep(delay);

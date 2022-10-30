9f4799019c7d969ebb5af62e1256d1d6854daf5b
TingHungChiu <113576156+TingHungChiu@users.noreply.github.com>
2022-10-30 09:20:25 -0400

    Update PolynomialBackoff.java

--- src/main/java/edu/vt/ece/hw4/backoff/PolynomialBackoff.java
+++ src/main/java/edu/vt/ece/hw4/backoff/PolynomialBackoff.java
@@ -9,7 +9,6 @@ public class PolynomialBackoff implements Backoff{
         a = count;
         e =Math.exp(1);
 
-        //System.out.println("a:"+count);
         int delay = (int) Math.pow(a,e);
         System.out.println("delay:"+delay);
         Thread.sleep(delay);
